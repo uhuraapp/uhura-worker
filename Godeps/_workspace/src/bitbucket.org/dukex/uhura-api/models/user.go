522c2cd2a584c6404cc6cd6446ce6a5a52517d2b
Duke <dukekhaos@gmail.com>
2015-10-16 09:46:36 -0300

    update uhura-api

--- Godeps/_workspace/src/bitbucket.org/dukex/uhura-api/models/user.go
+++ Godeps/_workspace/src/bitbucket.org/dukex/uhura-api/models/user.go
@@ -1,99 +1,124 @@
 package models
 
 import (
+	"encoding/json"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/dchest/uniuri"
-	"github.com/dukex/login2"
+	authenticator "github.com/dukex/go-auth"
 	"github.com/jinzhu/gorm"
 )
 
 type User struct {
 	Id            int64
 	Name          string
-	Link          string
-	Picture       string
-	Gender        string
-	Locale        string
-	GoogleId      string
 	Email         string `sql:"not null;unique"`
 	Password      string `sql:"type:varchar(100);"`
-	WelcomeMail   bool
+	Locale        string
 	CreatedAt     time.Time
+	LastVisitedAt time.Time
 	Provider      string `sql:"type:varchar(100);"`
 	ProviderId    string `sql:"type:varchar(50);"`
 	RememberToken string `sql:"type:varchar(100);unique"`
 	ApiToken      string `sql:"type:varchar(100);unique"`
-	LastVisitedAt time.Time
+	WelcomeMail   bool
+}
+
+type UserEntity struct {
+	Id         int64  `json:"id"`
+	Name       string `json:"name"`
+	Locale     string `json:"locale"`
+	Email      string `json:"email"`
+	ProviderId string `json:"provider_id"`
+	ApiToken   string `json:"token"`
 }
 
 func (self User) TableName() string {
 	return "users"
 }
 
-type UserHelpers struct {
+type UserHelper struct {
 	DB gorm.DB
 }
 
-func (h UserHelpers) SetupFromOAuth(provider string, u *login2.User, rawResponde *http.Response) (int64, error) {
-	user, err := h.findByEmail(u.Email)
+func (h *UserHelper) PasswordByEmail(email string) (string, bool) {
+	var u struct {
+		Password string
+	}
+
+	err := h.DB.Table(User{}.TableName()).
+		Select("password").
+		Where("email = ? ", email).Scan(&u).Error
+
+	if err != nil {
+		return "", false
+	}
+
+	return u.Password, true
+}
+func (h *UserHelper) FindUserDataByEmail(email string) (string, bool) {
+	var user UserEntity
+	err := h.DB.Table(User{}.TableName()).
+		Where("email = ? ", email).First(&user).Error
+
+	if err != nil {
+		return "", false
+	}
+
+	userJSON, err := json.Marshal(&user)
+
+	if err != nil {
+		return "", false
+	}
+
+	return string(userJSON), true
+}
+func (h *UserHelper) FindUserByToken(token string) (string, bool) {
+	var u struct {
+		Id string
+	}
+
+	err := h.DB.Table(User{}.TableName()).
+		Select("id").
+		Where("api_token = ? ", token).Scan(&u).Error
+
+	if err != nil {
+		return "", false
+	}
+
+	return u.Id, true
+}
+func (h *UserHelper) FindUserFromOAuth(provider string, user *authenticator.User, rawResponse *http.Response) (userID string, err error) {
+	var u struct {
+		Id string
+	}
+	err = h.DB.Table(User{}.TableName()).
+		Where("email = ?", user.Email).
+		First(&u).
+		Error
 	if err != nil {
 		if err == gorm.RecordNotFound {
-			return h.createFromOAuth(provider, u)
+			userData := User{
+				Email:      user.Email,
+				Password:   uniuri.NewLen(6),
+				Provider:   provider,
+				ProviderId: user.Id,
+				Locale:     user.Locale,
+				Name:       user.Name,
+				ApiToken:   user.Token,
+			}
+			err = h.DB.Table(User{}.TableName()).Save(&userData).Error
+			userID = strconv.Itoa(int(userData.Id))
 		}
-		return 0, err
 	} else {
-		return user.Id, nil
-	}
-}
-
-func (h UserHelpers) ByToken(token string) (int64, bool) {
-	var user User
-
-	if token == "" {
-		return 0, false
+		userID = u.Id
 	}
 
-	if CACHED_TOKEN[token] != 0 {
-		return CACHED_TOKEN[token], true
-	}
-
-	err := h.DB.Table(User{}.TableName()).Where("api_token = ?", token).First(&user).Error
-	if err != nil {
-		return 0, false
-	}
-
-	CACHED_TOKEN[token] = user.Id
-	return user.Id, true
-}
-
-//
-
-func (h UserHelpers) findByEmail(email string) (user User, err error) {
-	err = h.DB.Table(User{}.TableName()).Where("email = ?", email).First(&user).Error
 	return
 }
 
-func (h UserHelpers) createFromOAuth(provider string, temp *login2.User) (int64, error) {
-	user := User{
-		Email:      temp.Email,
-		Password:   uniuri.NewLen(6),
-		Provider:   provider,
-		ProviderId: temp.Id,
-		Link:       temp.Link,
-		Picture:    temp.Picture,
-		Locale:     temp.Locale,
-		Name:       temp.Name,
-		ApiToken:   temp.Token,
-	}
-	err := h.DB.Table(User{}.TableName()).Save(&user).Error
-
-	return user.Id, err
-}
-
-var CACHED_TOKEN map[string]int64
-
-func init() {
-	CACHED_TOKEN = make(map[string]int64, 0)
+func NewUserHelper(db gorm.DB) *UserHelper {
+	return &UserHelper{db}
 }

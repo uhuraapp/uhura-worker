cd18ff963e9a380d205e4b65f123bce8a8aec2b6
Duke <dukekhaos@gmail.com>
2015-11-19 08:52:53 -0200

    update deps

--- Godeps/_workspace/src/bitbucket.org/dukex/uhura-api/channels/create.go
+++ Godeps/_workspace/src/bitbucket.org/dukex/uhura-api/channels/create.go
@@ -12,15 +12,10 @@ import (
 
 func Create(database gorm.DB, url string) (*entities.Channel, bool) {
 	channelURL, _ := helpers.ParseURL(url)
-	channels, err := parser.URL(channelURL)
+	channelF, err := parser.URL(channelURL)
 
 	if err != nil {
 		log.Debug("error: %s", err)
-		return nil, false
-	}
-
-	if len(channels) < 1 {
-		log.Debug("error: no channel found")
 		return nil, false
 	}
 
@@ -28,7 +23,6 @@ func Create(database gorm.DB, url string) (*entities.Channel, bool) {
 	var channel entities.Channel
 
 	log.Debug("no error found")
-	channelF := channels[0] // TODO: fix it
 
 	log.Debug("channel UhuraID: %s", channelF.UhuraID)
 	if channelF.UhuraID != "" {
@@ -44,10 +38,42 @@ func Create(database gorm.DB, url string) (*entities.Channel, bool) {
 		if ok {
 			database.Table(models.Channel{}.TableName()).First(&channel, model.Id)
 			go CreateLinks(channelF.Links, channel.Id, database)
+			// TODO: add to sync queue
 		}
 	}
 
 	return &channel, ok
+}
+
+func TranslateFromFeedToEntity(entity entities.Channel, channel *parser.Channel) entities.Channel {
+	entity.Uri = channel.ID
+	entity.Title = channel.Title
+	entity.Description = channel.Description
+	entity.Copyright = channel.Copyright
+	entity.ImageUrl = channel.Image
+	entity.UpdatedAt = time.Now()
+	return entity
+}
+
+func TranslateEpisodesFromFeedToEntity(channel *parser.Channel) ([]*entities.Episode, []int64) {
+	episodes := make([]*entities.Episode, 0)
+	ids := make([]int64, 0)
+
+	for i, episode := range channel.Episodes {
+		s := int64(0)
+		id := int64(i) + time.Now().Unix()
+		episodes = append(episodes, &entities.Episode{
+			Id:          id,
+			Title:       episode.Title,
+			Description: episode.Description,
+			SourceUrl:   episode.Source,
+			StoppedAt:   &s,
+			ChannelUri:  channel.ID,
+		})
+		ids = append(ids, id)
+	}
+
+	return episodes, ids
 }
 
 func TranslateFromFeed(model models.Channel, channel *parser.Channel) models.Channel {
--- Godeps/_workspace/src/bitbucket.org/dukex/uhura-api/parser/main.go
+++ Godeps/_workspace/src/bitbucket.org/dukex/uhura-api/parser/main.go
@@ -6,101 +6,16 @@ import (
 	_ "code.google.com/p/go-charset/data"
 )
 
-func URL(url *url.URL) (channels []*Channel, _error error) {
+func URL(url *url.URL) (channels *Channel, _error error) {
 	log.Debug("creating fetcher")
 
-	channels = make([]*Channel, 0)
-
 	c := make(chan *Channel)
-	finish := make(chan bool)
 	err := make(chan error)
 
-	fetcher := NewFetcher([]string{url.String()}, c, finish, err)
+	go RunFetcher(url.String(), c, err)
 
-	go fetcher.run()
+	log.Debug("channels: %s", channels)
+	log.Debug("_error: %s", _error)
 
-	go func() {
-		_error = <-err
-		log.Error("%s", _error)
-		finish <- true
-	}()
-
-	go func() {
-		channel, ok := <-c
-		if ok {
-			channel.requestedURL = url.String()
-			log.Debug("finishing with %s", channel.Title)
-			channels = append(channels, channel)
-		} else {
-			log.Debug("finishing with error %s")
-		}
-	}()
-
-	<-finish
-
-	return channels, _error
+	return <-c, <-err
 }
-
-// func ProcessChannel(channel *rss.Channel) {
-// 	CacheImage(channel)
-// 	SaveOnDatabase(channel)
-// }
-//
-// func CacheImage(channel *rss.Channel) {
-// 	imageURL := GetChannelImageURL(channel)
-// 	originalImage, err := RequestURL(imageURL)
-//
-// 	log.Println(imageURL)
-// 	if err != nil {
-// 		//	notifyWrongImage
-// 	}
-// 	image, err := imageproxy.Transform(originalImage, resizeOptions())
-// 	if err != nil {
-// 		log.Println(err)
-// 	}
-//
-// 	str := base64.StdEncoding.EncodeToString(image)
-// 	fmt.Println(str)
-// }
-//
-
-// func resizeOptions() imageproxy.Options {
-// 	return imageproxy.ParseOptions("250x")
-// }
-//
-// func SaveOnDatabase(channel *rss.Channel) {
-// }
-
-////
-
-// func GetChannelImageURL(channel *rss.Channel) string {
-// 	var imageURL string
-// 	// 	if itunesImage := channel.Extensions[ITUNES_EXT]["image"]; itunesImage != nil {
-// 	// 		imageURL = itunesImage[0].Attrs["href"]
-// 	// 	} else {
-// 	imageURL = channel.Image.Url
-// 	// 	}
-// 	return imageURL
-// }
-
-////////
-
-// func GetDelayBetweenEpisodes() {
-//
-// }
-//
-// func SetNewRun() {
-// }
-
-// If isFeed
-//    RequestURL -> ParserXML
-//    FindChannel or CreateChannel
-//    UpdateChannel
-//    CacheImage
-//    FindEpisodes or CreateEpisodes
-//      SetContentType
-//      SetContentLengh
-//    GetDelayBetweenEpisodes
-//    SetNewRun
-// else
-//    FindChannelOnWebsite
